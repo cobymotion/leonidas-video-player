ce45e2cf3729ed503da842c8f96e8626bd473e3f
Luis Cobian <coby10@gmail.com>
2021-08-11 11:46:26 -0500

    Progress sincronizada con el player

--- video-player/js/index.js
+++ video-player/js/index.js
@@ -3,11 +3,16 @@ const playButton = document.querySelector('#play');
 const pauseButton = document.querySelector('#pause');
 const backwardButton = document.querySelector('#backward'); 
 const forwardButton = document.querySelector('#forward');
+const progreso = document.querySelector('#progress');
 
 playButton.addEventListener('click', handlePlay);
 pauseButton.addEventListener('click', handlePause);
 backwardButton.addEventListener('click', handleBackward);
 forwardButton.addEventListener('click', handleForward); 
+video.addEventListener('loadedmetadata', handleLoadedVideo);
+video.addEventListener('timeupdate', handleTimeUpdate);
+progreso.addEventListener('input', handleInput)
+
 
 function handlePlay(){
 	console.log("Se hizo el play")
@@ -31,4 +36,17 @@ function handleForward() {
 function handleBackward() {
 	console.log("Se retrocedio 10 segundos");
 	video.currentTime -= 10 
+}
+
+function handleLoadedVideo(){
+	console.log("Se cargo mi vide: ", video.duration);
+	progreso.max = video.duration;
+}
+
+function handleTimeUpdate(){
+	progreso.value = video.currentTime; 
+}
+
+function handleInput(){
+	video.currentTime = progreso.value
 }
